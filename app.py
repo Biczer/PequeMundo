12e099b360b4cfdac36965542f1f8f10e1ef4cca
ChrisDuoc <ch.fuenteso@duocuc.cl>
2026-05-12 12:25:37 -0400

    cambios de flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,40 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print("Correo login:", email)
+        print("Contraseña login:", password)
+
+        return redirect("/")
+
+    return render_template("login.html")
+
+
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        email = request.form["email"]
+        telefono = request.form["telefono"]
+        password = request.form["password"]
+        confirmar_password = request.form["confirmar_password"]
+
+        print("Nombre:", nombre)
+        print("Correo registro:", email)
+        print("Teléfono:", telefono)
+        print("Contraseña:", password)
+        print("Confirmar contraseña:", confirmar_password)
+
+        return redirect("/login")
+
+    return render_template("registro.html")
 
 
 if __name__ == "__main__":
