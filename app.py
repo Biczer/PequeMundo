3720c94d3ad0fb33fdf120d07225291f6ebd3274
ChrisDuoc <ch.fuenteso@duocuc.cl>
2026-06-03 15:35:41 -0400

    app.py para railway

--- app.py
+++ app.py
@@ -840,5 +840,6 @@ def mp_webhook():
     return jsonify({"status": "ok"}), 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
