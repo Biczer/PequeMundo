671d7f380758ff44f002c01a55986815d6c0ffee
ChrisDuoc <ch.fuenteso@duocuc.cl>
2026-06-18 15:25:58 -0400

    reorganizacion de prueba

--- py/detalle.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-
-def register_routes(app):
-
-    @app.route("/producto/<int:id>")
-    def detalle_producto(id):
-        return render_template("detalle.html")
