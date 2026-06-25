1956f531e3817ba465100af076766bf3e2c3840a
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 12:22:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,9 @@
 import os
-from urllib.parse import quote_plus
-
-_password = quote_plus('zZsidTQBUbMlHFdfwBVHJcLWCZuDesdw')
-
-
-import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MP_ACCESS_TOKEN = os.environ.get('MP_ACCESS_TOKEN')
 
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") + "?sslmode=require"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
