2c142e95b3dfaabca4318bfa5dd2607f97bd231c
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 17:14:32 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MP_ACCESS_TOKEN = os.environ.get('MP_ACCESS_TOKEN')
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "pequemundo_desarrollo_2026"
+    )
 
+    MP_ACCESS_TOKEN = os.environ.get("MP_ACCESS_TOKEN")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
