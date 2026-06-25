89154609cc20be72e5ffd3f81b82ac9ee4b79505
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 16:52:27 -0400

    Update usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -2,7 +2,7 @@ from extensions import db
 
 
 class Usuario(db.Model):
-    __tablename__ = 'usuario'
+    __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
