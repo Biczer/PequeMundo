32686d9e6621688bb776c15ce32e05936b091a50
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 14:55:07 -0400

    Update producto.py

--- models/producto.py
+++ models/producto.py
@@ -3,7 +3,7 @@ from extensions import db
 
 class Producto(db.Model):
     __tablename__ = 'productos'
-    id = db.Column(db.Integer, primary_key=True)
+    id_producto = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.String(300), nullable=False)
     imagen = db.Column(db.String(255), nullable=False)
