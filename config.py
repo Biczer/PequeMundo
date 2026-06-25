9fb977746e61dfc9b18a8a0012dddb264e23a612
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-24 20:54:38 -0400

    Refactor database URI and environment variables

--- config.py
+++ config.py
@@ -4,13 +4,17 @@ from urllib.parse import quote_plus
 _password = quote_plus('zZsidTQBUbMlHFdfwBVHJcLWCZuDesdw')
 
 
+import os
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-pequemundo-2024')
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://root:{_password}@acela.proxy.rlwy.net:42425/railway"
+    SECRET_KEY = os.environ.get(
+        'SECRET_KEY',
+        'dev-secret-pequemundo-2024'
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MP_ACCESS_TOKEN = os.environ.get(
         'MP_ACCESS_TOKEN',
         'TEST-7405494608123272-053123-046e81571c23bf9c73efb64662b94c28-585535158'
     )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
