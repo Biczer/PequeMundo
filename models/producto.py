cab06fd17bf0ec677e32131e07592c947341a8c8
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 15:03:01 -0400

    Update producto.py

--- models/producto.py
+++ models/producto.py
@@ -1,6 +1,14 @@
 from extensions import db
-from sqlalchemy import func
 
-productos = Producto.query.filter(
-    func.lower(Producto.estado) == "activo"
-).all()
+class Producto(db.Model):
+    __tablename__ = 'productos'
+
+    id_producto = db.Column(db.Integer, primary_key=True)
+
+    nombre = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(300))
+    imagen = db.Column(db.String(255))
+    categoria = db.Column(db.String(50), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    estado = db.Column(db.String(20), nullable=False)
