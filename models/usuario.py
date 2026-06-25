ef04b7ad66c9e217ffdcfe6e12825f9889dd14ee
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 16:53:43 -0400

    Update usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -3,9 +3,9 @@ from extensions import db
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    nombre_usuario = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
     telefono = db.Column(db.String(20), nullable=True)
+    password_hash = db.Column(db.String(200), nullable=False)
     rol = db.Column(db.String(20), nullable=False, default='Cliente')
