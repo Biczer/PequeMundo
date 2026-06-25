73b8605f5c1d0dbdfc22f746a8fe59441e692d12
lusiano1234 <lu.osoriov@duocuc.cl>
2026-06-25 18:15:40 -0400

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -1,13 +1,41 @@
 from extensions import db
 
+
 class Pedido(db.Model):
+
     __tablename__ = 'pedido'
 
-    id_usuario = db.Column(db.Integer, primary_key=True)
-    cliente = db.Column(db.String(100), nullable=False)
-    cliente_email = db.Column(db.String(100), nullable=True)
-    total = db.Column(db.Float, nullable=False)
-    estado = db.Column(db.String(30), nullable=False, default='Pendiente')
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+
+    cliente = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+
+    cliente_email = db.Column(
+        db.String(100),
+        nullable=True
+    )
+
+
+    total = db.Column(
+        db.Float,
+        nullable=False
+    )
+
+
+    estado = db.Column(
+        db.String(30),
+        nullable=False,
+        default='Pendiente'
+    )
+
 
     vendedor_id = db.Column(
         db.Integer,
@@ -15,10 +43,30 @@ class Pedido(db.Model):
         nullable=True
     )
 
-    mp_preference_id = db.Column(db.String(200), nullable=True)
-    mp_payment_id = db.Column(db.String(200), nullable=True)
-    mp_status = db.Column(db.String(50), nullable=True)
-    items_json = db.Column(db.Text, nullable=True)
+
+    mp_preference_id = db.Column(
+        db.String(200),
+        nullable=True
+    )
+
+
+    mp_payment_id = db.Column(
+        db.String(200),
+        nullable=True
+    )
+
+
+    mp_status = db.Column(
+        db.String(50),
+        nullable=True
+    )
+
+
+    items_json = db.Column(
+        db.Text,
+        nullable=True
+    )
+
 
     vendedor = db.relationship(
         'Usuario',
